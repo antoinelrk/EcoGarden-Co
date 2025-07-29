bbc64499add589932dfbde4ea0d9e84c122b9c5d
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-07-29 06:42:41 +0200

    Refactor AppFixtures to use Advice entity and enhance data loading logic

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,7 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Conseil;
+use App\Entity\Advice;
 use App\Enums\MonthEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,13 +11,15 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-//        for ($i = 1; $i <= 100; $i++) {
-//            $advice = new Conseil();
-//            $advices[] = [
-//                'advice' => "Advice number $i",
-//                'months' => MonthEnum::randomEnum(5)
-//            ];
-//        }
+        for ($i = 1; $i <= 100; $i++) {
+            $advice = new Advice();
+            $advice->setTitle("Advice Title $i")
+                   ->setDescription("This is the description for advice number $i.")
+                    ->setMonths(MonthEnum::randomEnum(5)) // Assuming randomEnum returns an array of MonthEnum values
+                   ->setCreatedAt(new \DateTimeImmutable())
+                   ->setUpdatedAt(new \DateTimeImmutable());
+            $manager->persist($advice);
+        }
 
         $manager->flush();
     }
