fa6366418bf02c70bf782d2e32a7002566a19ea9
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-08-01 07:10:16 +0200

    Add user fixtures for admin and regular user with hashed passwords

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,27 @@
 namespace App\DataFixtures;
 
 use App\Entity\Advice;
+use App\Entity\User;
 use App\Enums\MonthEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        protected UserPasswordHasherInterface $passwordHasher
+    ) {}
+
+    /**
+     * Load data fixtures with the passed EntityManager.
+     *
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager): void
     {
+        $this->loadUsers($manager);
+
         for ($i = 1; $i <= 5; $i++) {
             $advice = new Advice();
             $advice->setTitle("Advice Title $i")
@@ -22,5 +35,30 @@ class AppFixtures extends Fixture
         }
 
         $manager->flush();
+    }
+
+    private function loadUsers(ObjectManager $manager): void
+    {
+        $users = [
+            [
+                'email' => 'admin@acogarden.co',
+                'password' => 'P@ss1234',
+                'roles' => ['ROLE_ADMIN'],
+            ],
+            [
+                'email' => 'user@acogarden.co',
+                'password' => 'P@ss1234',
+                'roles' => ['ROLE_USER'],
+            ]
+        ];
+
+        foreach ($users as $userData) {
+            $user = new User();
+            $user->setEmail($userData['email'])
+                 ->setRoles($userData['roles'])
+                 ->setPassword($this->passwordHasher->hashPassword($user, $userData['password']));
+
+            $manager->persist($user);
+        }
     }
 }
