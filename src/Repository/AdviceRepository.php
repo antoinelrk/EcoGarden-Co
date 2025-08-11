94f9bff6bd8a650d21c3513dfce2585988f0ac39
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-08-11 06:22:11 +0200

    Add create method to AdviceRepository for handling advice creation

--- src/Repository/AdviceRepository.php
+++ src/Repository/AdviceRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Advice;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use JsonException;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -14,8 +16,10 @@ class AdviceRepository extends ServiceEntityRepository
 {
     public const int MAX_PAGE = 10;
 
-    public function __construct(ManagerRegistry $registry)
-    {
+    public function __construct(
+        ManagerRegistry $registry,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
         parent::__construct($registry, Advice::class);
     }
 
@@ -30,6 +34,30 @@ class AdviceRepository extends ServiceEntityRepository
             ->setMaxResults($limit)
             ->getQuery()
             ->getResult();
+    }
+
+    /**
+     * Creates a new advice.
+     *
+     * @throws JsonException
+     */
+    public function create(Request $request): Advice
+    {
+        $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
+        $advice = new Advice();
+        $advice->setTitle($data['title']);
+        $advice->setDescription($data['description']);
+
+        $months = array_map('intval', $data['months']);
+        $advice->setMonths($months);
+
+        $advice->setCreatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($advice);
+        $this->entityManager->flush();
+
+        return $this->serializer->serialize($advice, 'json');
     }
 
 //    /**
